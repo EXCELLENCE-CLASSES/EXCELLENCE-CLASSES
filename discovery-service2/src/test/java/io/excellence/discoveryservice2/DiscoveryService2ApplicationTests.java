b33dad683cb426cff16a7363e4fd6792b84c4104
EXCELLENCE-CLASSES <96632018+EXCELLENCE-CLASSES@users.noreply.github.com>
2022-01-09 03:09:59 +0530

    Add files via upload

---
+++ discovery-service2/src/test/java/io/excellence/discoveryservice2/DiscoveryService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.excellence.discoveryservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
