e32e051c1a720380dbf177252b4bcbd674e9758d
EXCELLENCE-CLASSES <96632018+EXCELLENCE-CLASSES@users.noreply.github.com>
2022-01-16 10:11:40 +0530

    Add files via upload

---
+++ JavaProgramPractice/src/test/java/com/excellence/classes/GarbageColletor1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.classes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageColletor1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
