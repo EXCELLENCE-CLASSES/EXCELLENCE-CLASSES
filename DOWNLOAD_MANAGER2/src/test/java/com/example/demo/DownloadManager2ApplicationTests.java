94962e863a60a9a787aee757fdeea2059cbfc746
EXCELLENCE CLASSES <aryaritesh207@gmail.com>
2022-03-04 02:46:48 +0530

    Add files via upload

--- DOWNLOAD_MANAGER2/src/test/java/com/example/demo/DownloadManager2ApplicationTests.java
+++ DOWNLOAD_MANAGER2/src/test/java/com/example/demo/DownloadManager2ApplicationTests.java
@@ -1,18 +1,28 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace=Replace.NONE)
 class DownloadManager2ApplicationTests {
+	
+	@Autowired
+	private ExcellenceDocumentRepository repo; 
+	
+	@Autowired
+	private TestEntityManager entityManager ;
 
 	@Test
 	void testInsertDocument() {
@@ -23,11 +33,18 @@ class DownloadManager2ApplicationTests {
 		try {
 			byte[] bytes=Files.readAllBytes(file.toPath());
 			exc.setContent(bytes);
-			exc.setSize(bytes.length);
+			long fileSize=bytes.length;
+			exc.setSize(fileSize);
+			ExcellenceDocument savDoc=repo.save(exc);
+			ExcellenceDocument existDoc=entityManager.find(ExcellenceDocument.class, savDoc.getId());
+			assertThat(existDoc.getSize()).isEqualTo(fileSize);
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 		
 	}
 
