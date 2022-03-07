fa5d97f37460d5fba5b2c85d9c9ba0cdc073fbe0
EXCELLENCE CLASSES <aryaritesh207@gmail.com>
2022-03-07 21:34:36 +0530

    Add files via upload

---
+++ DOWNLOAD_MANAGER/src/main/java/com/example/demo/DownloadManagerApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadManagerApplication.class, args);
+		System.out.println("Welcome To Excellence Classes");
+	}
+
+}
