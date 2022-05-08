a459896cd715beb7a63e9f9e13917fe24c61d144
EXCELLENCE CLASSES <aryaritesh207@gmail.com>
2022-05-08 22:39:22 +0530

    Add files via upload

---
+++ JavaProgramPractice2-1/src/main/java/com/example/demo/JavaProgramPractice21Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProgramPractice21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProgramPractice21Application.class, args);
+	}
+
+}
